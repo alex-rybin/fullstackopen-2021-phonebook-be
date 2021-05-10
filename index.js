e3181b45dc8ef463cd7cf8fb53f77080067c5b1a
Alexander Rybin <alex_rybin@hotmail.com>
2021-05-10 19:57:15 +0300

    Add exercise 3.16

--- index.js
+++ index.js
@@ -37,33 +37,40 @@ let persons = [
     }
 ]
 
-app.get('/persons/', (request, response) => {
+const errorHandler = (error, req, res) => {
+    console.error(error.message)
+
+    return error => res.status(500).json({error})
+}
+
+
+app.get('/persons/', (req, res, next) => {
     Person.find()
-        .then(result => response.json(result))
-        .catch(error => response.status(500).json({error}))
+        .then(result => res.json(result))
+        .catch(error => next(error))
 })
 
-app.get('/persons/:id/', (request, response) => {
-    const person = persons.find(person => person.id === Number(request.params.id))
+app.get('/persons/:id/', (req, res) => {
+    const person = persons.find(person => person.id === Number(req.params.id))
 
     if (person) {
-        response.json(person)
+        res.json(person)
     } else {
-        response.status(404).send()
+        res.status(404).send()
     }
 })
 
-app.delete('/persons/:id/', ((req, res) => {
+app.delete('/persons/:id/', ((req, res, next) => {
     Person.findByIdAndDelete(req.params.id)
         .then(result => res.status(204).send())
-        .catch(error => res.status(500).json({error}))
+        .catch(error => next(error))
 }))
 
-app.get('/info/', (request, response) => {
-    response.send(`Phonebook has info for ${persons.length} people<br/>${new Date().toString()}`)
+app.get('/info/', (req, res) => {
+    res.send(`Phonebook has info for ${persons.length} people<br/>${new Date().toString()}`)
 })
 
-app.post('/persons/', ((req, res) => {
+app.post('/persons/', ((req, res, next) => {
     if (!req.body.name) {
         return res.status(400).json({error: 'Name is missing'})
     } else if (!req.body.number) {
@@ -72,9 +79,11 @@ app.post('/persons/', ((req, res) => {
 
     const person = new Person(req.body)
 
-    person.save().then(savedPerson => {
-        res.json(savedPerson)
-    })
+    person.save()
+        .then(savedPerson => res.json(savedPerson))
+        .catch(error => next(error))
 }))
+
+app.use(errorHandler)
 
 app.listen(process.env.PORT || 3001)
